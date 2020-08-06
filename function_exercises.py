04ea624db985f2f02a21a016c078db987bae2940
Samuel Davila <samuelrdavila@gmail.com>
2020-08-06 15:45:03 -0500

    updated comment format

--- function_exercises.py
+++ function_exercises.py
@@ -8,11 +8,14 @@
 # the number or the string 2, False otherwise.
 
 def is_two(x):
-    assert type(x) == str or type(x) == int, 'argument passed is not a string or integer '  # check if x is a str or int
+    # check if x is a str or int
+    assert type(x) == str or type(x) == int, 'argument passed is not a string or integer '  
     
-    if x != 2 and x != '2':                                # if x is not 2 or '2', return false
+    # if x is not 2 or '2', return false
+    if x != 2 and x != '2':                                
         return False
-    elif x == 2 or x == '2':                               # if x is 2 or '2', return true with a cool statement
+     # if x is 2 or '2', return true with a cool statement
+    elif x == 2 or x == '2':                            
         return True
 
 is_two('2')
@@ -24,19 +27,25 @@ is_two('2')
 # Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
 
 def is_vowel(s):
-    assert type(s) == str, 'argument passed is not a string' # check that argument is a string
+    # check that argument is a string
+    assert type(s) == str, 'argument passed is not a string' 
     
-    vowels = ['a','e','i','o','u']
-    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+    # create variable that holds list of vowels
+    vowels = ",".join('aeiou')
+    # create variable that holds list of consonants
+    consonants = ",".join('bcdfghjklmnpqrstvwxyz')
     
-    if s.lower() in vowels:  # using list of single vowels to ensure double vowels (ae, iu, etc) dont pass check
+    # using list of single vowels to ensure double vowels (ae, iu, etc) dont pass check
+    if s.lower() in vowels:  
         return True
-    elif s.lower() in consonants:    # using list of consontants to avoid double consonants being passed as a single consonant in next exercise
+    # using list of consontants to avoid double consonants being passed as a single consonant in next exercise
+    elif s.lower() in consonants:   
         return False   
+    # print message telling user they entered a string with multiple characters if neither of the other two conditions are met  
     else:
         return(f'{s} is a string of multiple characters, please pass a single character')
-    
-is_vowel('a')
+ 
+is_vowel('u')
 
 
 # In[86]:
@@ -44,11 +53,14 @@ is_vowel('a')
 
 # Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
 
-def is_consonant(s):           # no assertion line necessary here bc is_vowel already has one
-    if is_vowel(s) == False:   # running is_vowel to check if s is a vowel
+def is_consonant(s):
+    # no assertion line necessary here bc is_vowel already has one  
+    # running is_vowel to check if s is a vowel         
+    if is_vowel(s) == False:   
         return True
+    # returning false if not a consonant
     else:
-        return False           # returning false if not a consonant
+        return False           
     
 is_consonant('v')
 
@@ -60,12 +72,16 @@ is_consonant('v')
 # The function should capitalize the first letter of the word if the word starts with a consonant.
 
 def capital_consonant(s):
-    assert type(s) == str, 'argument passed is not a string' # check if argument is a string
+    # check if argument is a string
+    assert type(s) == str, 'argument passed is not a string' 
     
-    if s[0].lower() not in 'aeiou': # check if first letter is a vowel. alternative is to remove .lower and add 'AEIOU' to string    
-        return s.capitalize()       # return string with capitalized first letter if first letter is a consonant
+    # check if first letter is a vowel. alternative is to remove .lower and add 'AEIOU' to string 
+    if s[0].lower() not in 'aeiou': 
+    # return string with capitalized first letter if first letter is a consonant   
+        return s.capitalize()  
+    # return string as normal if first letter is a vowel     
     else:
-        return(s)                   # return string as normal if first letter is a vowel
+        return(s)                  
         
 capital_consonant('cat')
 
@@ -76,13 +92,18 @@ capital_consonant('cat')
 # Define a function named calculate_tip. 
 #It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
 
-def calculate_tip(tp, bt):   # tip percentage and bill total set as parameters
-    assert type(tp) == int or type(tp) == float, 'tip percent argument is not float or int' # assert arguments are correct type, float and int on both arguments in case of bill less than $1 or 100% tip
+# tip percentage and bill total set as parameters
+def calculate_tip(tp, bt):   
+    # assert arguments are correct type, float and int on both arguments in case of bill less than $1 or 100% tip
+    assert type(tp) == int or type(tp) == float, 'tip percent argument is not float or int' 
     assert type(bt) == int or type(bt) == float, 'bill total argument is not float or int'
-    assert tp < 1 or bt < 1, 'tip percentage argument not between 0 and 1'                  # assert tip percentage is between 0 and 1 since the arguments in interchangable, only one needs to between 1 and 0
+    # assert tip percentage is between 0 and 1 since the arguments in interchangable, only one needs to between 1 and 0
+    assert tp < 1 or bt < 1, 'tip percentage argument not between 0 and 1'                  
     
-    amount_to_tip = tp * bt  # multiply the values that are passed to find amount to tip
-    return amount_to_tip     # return amount to tip
+    # multiply the values that are passed to find amount to tip
+    amount_to_tip = tp * bt
+    # return amount to tip  
+    return amount_to_tip     
 
 calculate_tip(.05, 100)
 
@@ -93,13 +114,18 @@ calculate_tip(.05, 100)
 # Define a function named apply_discount. 
 # It should accept a original price, and a discount percentage, and return the price after the discount is applied.
 
-def apply_discount(op, dp):                # original price (op) and discount percentage (dp) set as parameters
-    assert type(op) == int or type(op) == float, 'original price argument is not float or int' # assert arguments are correct type, float and int on both arguments in case original price less than $1 or 100% discount
+ # original price (op) and discount percentage (dp) set as parameters
+def apply_discount(op, dp):                
+    # assert arguments are correct type, float and int on both arguments in case original price less than $1 or 100% discount
+    assert type(op) == int or type(op) == float, 'original price argument is not float or int' 
     assert type(dp) == int or type(dp) == float, 'discount percentage argument is not float or int'
-    assert dp >= 0 and dp <= 1, 'discount percentage, dp (right argument), must be between 0 and 1'   # assert discount argument between 0 and 1
+     # assert discount argument between 0 and 1
+    assert dp >= 0 and dp <= 1, 'discount percentage, dp (right argument), must be between 0 and 1'  
     
-    price_after_discount = op - (op * dp)  # op * dp to find disc amount then subtracted from op to find disc price
-    return(price_after_discount)           # return disc price
+    # op * dp to find disc amount then subtracted from op to find disc price
+    price_after_discount = op - (op * dp)  
+    # return disc price
+    return(price_after_discount)           
 
 apply_discount(50, .1)
 
@@ -111,12 +137,16 @@ apply_discount(50, .1)
 # It should accept a string that is a number that contains commas in it as input, and return a number as output.
 
 def handle_commas(s):   
-    assert type(s) == str, 'argument must be in string format' # check that argument type is str
+    # check that argument type is str
+    assert type(s) == str, 'argument must be in string format' 
     
-    no_commas = float(s.replace("," , ""))   # using .replace to remove commas and int() to convert string to int and store as variable   
-    return (no_commas)   # return new variable
- 
-handle_commas('4,000,000.25') # float type allows decimals to be preserved
+    # using .replace to remove commas and int() to convert string to int and store as variable   
+    no_commas = float(s.replace("," , ""))  
+    # return new variable 
+    return (no_commas)   
+
+# float type allows decimals to be preserved 
+handle_commas('4,000,000.25') 
 
 
 # In[143]:
@@ -126,10 +156,13 @@ handle_commas('4,000,000.25') # float type allows decimals to be preserved
 # It should accept a number and return the letter grade associated with that number (A-F).
 
 def get_letter_grade(i):
-    assert type(i) == int or type(i) == float, 'argument must be in int format' # check if argument is int or float type
+    # check if argument is int or float type
+    assert type(i) == int or type(i) == float, 'argument must be in int format' 
     
-    if int(i) >= 88:     # set if and elif statements to return letter grade based on argument passed
-        return 'A'       # using int(i) in case grade passed is grade in string form
+    # set if and elif statements to return letter grade based on argument passed
+    # using int(i) in case grade passed is grade in string form
+    if int(i) >= 88:     
+        return 'A'       
     elif int(i) >= 80:
         return 'B'
     elif int(i) >= 67:
@@ -148,12 +181,17 @@ get_letter_grade(95)
 # Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
 
 def remove_vowels(s):
-    assert type(s) == str, 'argument must be in str format' # check if argument is str type
+    # check if argument is str type
+    assert type(s) == str, 'argument must be in str format' 
     
-    no_vowels = ""                            # create empty string
-    for letter in s:                          # iterate through string
-        if letter.lower() not in 'aeiou':     # check if letter is a vowel
-            no_vowels += letter               # if not a vowel, add letter to empty string variable 
+    # create empty string
+    no_vowels = ""                
+    # iterate through string            
+    for letter in s:            
+        # check if letter is a vowel              
+        if letter.lower() not in 'aeiou':
+            # if not a vowel, add letter to empty string variable      
+            no_vowels += letter               
     return(no_vowels)               
     
 remove_vowels('banana')
@@ -175,13 +213,18 @@ First Name will become first_name
 """
 
 def normalize_name(s):
-    assert type(s) == str, 'argument must be in str format' # check if argument is str type
+    # check if argument is str type
+    assert type(s) == str, 'argument must be in str format' 
     
-    new_string = (s.strip(" ").lstrip("0123456789").lower().replace(" ", "_"))   # remove whitespace from ends, numbers from left side, lower case all, replace spaces in string with "_"
-    string2 = ""                                                      # create empty string
+    # remove whitespace from ends, numbers from left side, lower case all, replace spaces in string with "_"
+    new_string = (s.strip(" ").lstrip("0123456789").lower().replace(" ", "_"))  
+    # create empty string 
+    string2 = ""                                                      
     for c in new_string:
-        if c not in "!@#$%^&*()+=-[]{}\/|?.<>,`~":                    # remove special characters
-            string2 += c                                              # fill empty string with remaining characters
+        # remove special characters
+        if c not in "!@#$%^&*()+=-[]{}\/|?.<>,`~": 
+            # fill empty string with remaining characters                   
+            string2 += c                                              
     return(string2)
 
 normalize_name(' 123R $( )Abc2 ')
@@ -196,12 +239,17 @@ normalize_name(' 123R $( )Abc2 ')
 # cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
 
 def cumalative_sum(l):
-    assert type(l) == list, 'argument must be in list format' # check if argument is list type
-    running_total = 0                                         # create variable with value of zero
-    n = []                                                    # create empty list
+    # check if argument is list type
+    assert type(l) == list, 'argument must be in list format' 
+      # create variable with value of zero
+    running_total = 0                               
+    # create empty list        
+    n = []                                                    
     for i in l:
-        running_total += i                                    # use iteration to keep running total
-        n.append(running_total)                               # append values to empty list by adding running total to i
+         # use iteration to keep running total
+        running_total += i   
+          # append values to empty list by adding running total to i                                
+        n.append(running_total)                             
     return(n)
         
 cumalative_sum([1,2,3,4])
@@ -216,13 +264,17 @@ cumalative_sum([1,2,3,4])
 # representation of the time in a 24-hour format. 
 # Bonus write a function that does the opposite.
 
-from datetime import datetime                               # importing datetime for use in function
+# importing datetime for use in function
+from datetime import datetime                               
 
 def twelveto24(t):
-    assert type(t) == str, 'argument must be in str format' # check if argument is string type 
+    # check if argument is string type 
+    assert type(t) == str, 'argument must be in str format' 
     
-    time_convert = datetime.strptime(t, "%I:%M%p")          # convert argument to raw 24 hour format
-    only_time = datetime.strftime(time_convert, "%H:%M")    # specify format HH:MM 
+    # convert argument to raw 24 hour format
+    time_convert = datetime.strptime(t, "%I:%M%p")       
+     # specify format HH:MM    
+    only_time = datetime.strftime(time_convert, "%H:%M")   
     return(only_time)
 
 twelveto24("3:45pm")
@@ -234,12 +286,16 @@ twelveto24("3:45pm")
 
 # Bonus write a function that does the opposite
 
-from datetime import datetime                          # importing datetime for use in function
+# importing datetime for use in function
+from datetime import datetime                          
 
 def twentyfourto12(t): 
-    assert type(t) == str, 'argument must be in str format' # check if argument is string type  
-    time_convert = datetime.strptime(t,"%H:%M")        # convert to non-24 hour format
-    return time_convert.strftime("%I:%M%p").lower()    # specify format HH:MMpm or HH:MMam
+    # check if argument is string type 
+    assert type(t) == str, 'argument must be in str format' 
+    # convert to non-24 hour format 
+    time_convert = datetime.strptime(t,"%H:%M")       
+    # specify format HH:MMpm or HH:MMam 
+    return time_convert.strftime("%I:%M%p").lower()    
 
 twentyfourto12("22:30")
 
